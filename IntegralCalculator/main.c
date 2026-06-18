ad5a4b93d5d4c61ca650f299523f7ab09f84a583
AnaRianOwO <amrianocaro5@gmail.com>
2026-06-17 20:52:38 -0500

    Principios de la calculadora

--- IntegralCalculator/main.c
+++ IntegralCalculator/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+int suma(int a, int b){
+  return a+b;
+}
+
+int resta(int a, int b){
+  return a-b;
+}
+
+float division(float a, float b){
+  return a/b;
+}
+
+int multiplicacion(int a, int b){
+  return a*b;
+}
+
 int main() {
-    printf("¡Hola, bestie! Esta calculadora será épica uwu\n");
-    return 0;
+  printf("¡Hola, bestie! Esta calculadora será épica uwu\n");
+  int x, y;
+  scanf("%d\n", &x);
+  scanf("%d", &y);
+
+  int resultado = suma(x, y);
+  printf("La suma es:%d\n", resultado);
+  resultado = resta(x,y);
+  printf("La resta es:%d\n", resultado);
+  float resul = division(x,y);
+  printf("La división es:%f\n", resul);
+  resultado = multiplicacion(x,y);
+  printf("La multiplicación es:%d\n", resultado);
+
+  return 0;
 }
