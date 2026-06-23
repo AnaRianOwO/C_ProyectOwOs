36f587ee92c658244a6333d8f320b4b42222f1cc
AnaRianOwO <amrianocaro5@gmail.com>
2026-06-22 21:19:15 -0500

    adelantadito uwu

--- IntegralCalculator/main.c
+++ IntegralCalculator/main.c
@@ -36,9 +36,45 @@ void agregar_termino(struct Polinomio *p, float coef, int exp){
   p->num_terminos++;
 }
 
+void imprimir_polinomio(struct Polinomio p) {
+  for (int i=0; i<p.num_terminos; i++){
+    if (p.terminos[i].coeficiente == 0){
+      printf(".");
+    };
+    if (i != 0) && (p.terminos[i].coeficiente > 0){
+      printf("+");
+    }
+      //printf("%g%c^%d ", p.terminos[i].coeficiente, p.variable, p.terminos[i].exponente);
+  }
+  printf("El polinomio se ha imprimido correctamente ");
+}
+
+
+
+void comparar_e_intercambiar(struct Polinomio *p, int i){
+  if(p->terminos[i].exponente < p->terminos[i+1].exponente){
+        struct Termino temp = p->terminos[i];
+        p->terminos[i] = p->terminos[i+1];
+        p->terminos[i+1] = temp;
+      }
+}
+
+void organizar_polinomio(struct Polinomio *p){
+  for (int i=0; i<p->num_terminos-1; i++){
+    for (int j=0; j<p->num_terminos-1; j++){
+      comparar_e_intercambiar(p, j);
+    }
+  }
+}
+
 int main() {
-  struct Polinomio p = crear_polinomio('y', 0);
-  agregar_termino(&p, -8, 3);
-  printf("%f %c^%d. El polinomio tiene %d", p.terminos[0].coeficiente, p.variable, p.terminos[0].exponente, p.num_terminos);
+  struct Polinomio p = crear_polinomio('x', 0);
+  agregar_termino(&p, 8, 1);
+  agregar_termino(&p, -4, 3);
+  agregar_termino(&p, 10, 2);
+  agregar_termino(&p, -2, 4);
+  agregar_termino(&p, 1, 6);
+  organizar_polinomio(&p);
+  imprimir_polinomio(p);
   return 0;
 }
